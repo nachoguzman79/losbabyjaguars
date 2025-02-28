b0ced2db69401c15f2a9fc1a50d6174e482f1d86
nachoguzman79 <ignacioguzmanok@gmail.com>
2025-02-28 19:14:48 +0100

    gap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="relative max-w-7xl lg:mt-[80px] lg:mx-auto px-1 h-auto flex flex-col items-center">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 w-full">
+      <div className="grid grid-cols-1 lg:grid-cols-2 w-full">
         {/* Columna de imagen */}
         <div className="flex justify-center items-center order-1 lg:order-2">
           <img
@@ -14,7 +14,7 @@ const Home = () => {
         </div>
 
         {/* Columna de texto */}
-        <div className="flex flex-col justify-center items-start text-center lg:text-left order-2 lg:order-1">
+        <div className="  flex flex-col justify-center items-start text-center lg:text-left order-2 lg:order-1">
           <h1 className="text-7xl lg:text-7xl xl:text-9xl font-extrabold mb-4 tracking-tight text-left">
             LOS BABY JAGUARS
           </h1>
--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -3,20 +3,16 @@ import musicPageData from './musicPageData'; // Importamos la data desde su arch
 
 const Music = () => {
   return (
-    <div className="h-auto min-w-screen flex flex-col items-center justify-center p-2 md:p-14">
+    <div className="h-auto min-w-screen flex flex-col items-center justify-center p-2 md:p-14 ">
      
-      <div className="grid grid-cols-1 md:grid-cols-2  w-full max-w-5xl">
+      <div className=" grid grid-cols-1 md:grid-cols-2  w-full max-w-5xl gap-0">
         {musicPageData.map((item, index) => (
-          <div key={index} className=" md:p-6 p-2 rounded-lg flex flex-col items-center">
-            {/* <img
-              src={item.image}
-              alt={`Music cover ${index + 1}`}
-              className="w-full h-auto rounded-lg mb-4"
-            /> */}
+          <div key={index} className=" md:p-3 p-1 rounded-lg flex flex-col items-center">
+        
             <iframe
               title={`Bandcamp Player ${index + 1}`}
-              style={{ border: 0, width: "100%" }}
-              className="h-[500px] md:h-[600px]" // size of each iframe
+              style={{ border: 0, width: "100%", marginBottom: "0px" }}
+              className="h-[500px] md:h-[450px] lg:h-[700px] m-8 p-0" // size of each iframe
               src={item.url}
               seamless
             ></iframe>
